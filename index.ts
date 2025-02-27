26ffb3f4f208ae09d09cbd91bac15b1629ce137b
Mahdi Jafari <m98j77@gmail.com>
2025-02-27 13:31:58 +0330

    chat entity exported

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 export { Button } from "./src/components/Button";
 export { MessageEntity } from "./src/domain/MessageEntity";
 export { default as Chat } from "./src/features/Chat";
-export { AttachmentTypeEnum } from "./src/ts/enum";
+export { AttachmentTypeEnum, ChatThemEntity } from "./src/ts/enum";
 export type { SymbolAssignmentInterface } from "./src/ts/interfaces";
